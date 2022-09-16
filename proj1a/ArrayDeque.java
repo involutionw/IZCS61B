1e73e80ac3994165f66c15c364bb276250931138
involutionw <2651419874@qq.com>
2022-09-16 16:44:54 +0800

    debug ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,61 +1,79 @@
 public class ArrayDeque<T> {
     private T[] Array;
     private int size;
+    private int length;
     private int nextFirst;
     private int nextLast;
-    private double usage;
     public ArrayDeque() {
         Array = (T[])new Object[8];
         size = 0;
+        length = 8;
         nextFirst = 0;
         nextLast = 7;
-        usage = 0;
     }
 
-    private void resize(int res) {
-        T[] a = (T[]) new Object[res];
-        int l = res-(Array.length-1-nextLast), r = Array.length-1-nextLast;
-        System.arraycopy(Array, 0, a, 0, nextFirst);
-        System.arraycopy(Array, nextLast+1, a, res-(Array.length-1-nextLast), Array.length-1-nextLast);
-        Array = a;
-        nextLast = l-1;
-        updateusage();
+    private int minusOne(int x) {
+        if(x == 0) return Array.length-1;
+        else return x-1;
     }
-    private void checkSize(){
-        if(size() == Array.length) {
-            resize(Array.length * 2);
-        }else if(Array.length>=100 && usage<=0.25) {
-            resize(size()/2);
+
+    private int addOne(int x, int mod) {
+        return x+1 % mod;
+    }
+
+    /* 无需特判，因为expand时 F L一定相邻
+     *
+     */
+    private void expand() {
+        T[] a = (T[]) new Object[length*2];
+
+        int ptr1 = minusOne(nextFirst);
+        int ptr2 = minusOne(nextFirst);
+
+        for(; ptr2 != nextLast; ) {
+            a[ptr1] = Array[ptr2];
+            ptr2 = minusOne(ptr2);
+            if (ptr1 == 0) ptr1 = a.length - 1;
+            else ptr1--;
         }
+        nextLast = ptr1;
+        Array = a;
+        length = a.length;
     }
 
-    private void checksentinel() {
-        if(nextFirst < 0) nextFirst = Array.length-1;
-        else if(nextFirst == Array.length) nextFirst = 0;
+    private void shrink() {
+        T[] a = (T[]) new Object[length / 2];
 
-        if(nextLast == Array.length) nextLast = 0;
-        else if(nextLast < 0) nextLast = Array.length-1;
-    }
-    private void updateusage() {
-        usage = (double)size()/ Array.length;
-    }
+        int ptr1 = (length / 4) - 1;
+        int ptr2 = minusOne(nextFirst);
 
+        for(; ptr2 != nextLast; ) {
+            a[ptr1] = Array[ptr2];
+            ptr2 = minusOne(ptr1);
+            if (ptr1 == 0) ptr1 = a.length - 1;
+            else ptr1--;
+        }
+        nextLast = ptr1;
+        nextFirst = length / 4;
+        Array = a;
+        length = a.length;
+    }
     public void addFirst(T item) {
-        checkSize();
+        if(size == length - 1) {
+            expand();
+        }
         Array[nextFirst] = item;
-        nextFirst ++;
+        nextFirst = addOne(nextFirst, length);
         size ++;
-        checksentinel();
-        updateusage();
     }
 
     public void addLast(T item) {
-        checkSize();
+        if(size == length - 1) {
+            expand();
+        }
         Array[nextLast] = item;
-        nextLast --;
+        nextLast = minusOne(nextLast);
         size ++;
-        checksentinel();
-        updateusage();
     }
 
     public boolean isEmpty() {
@@ -68,31 +86,37 @@ public class ArrayDeque<T> {
 
     public void printDeque() {
         int ponint = nextFirst-1;
-        for(;ponint != nextLast; ponint --) {
-            if(ponint < 0) ponint = Array.length-1;
+        for(;ponint != nextLast; ponint = minusOne(ponint)) {
             System.out.println(Array[ponint]);
         }
     }
 
     public T removeFirst() {
-        nextFirst --;
-        checksentinel();
+        if(size == 0) {
+            return null;
+        }
+        double usage = (1.0*size / length);
+        if(length >= 16 && usage <= 0.25) {
+            shrink();
+        }
+        nextFirst = minusOne(nextFirst);
         T tmp = Array[nextFirst];
         Array[nextFirst] = null;
         size --;
-        updateusage();
-
         return tmp;
     }
 
     public T removeLast() {
-        nextLast ++;
-        checksentinel();
+        if(size == 0) {
+            return null;
+        }
+        if(length >= 16 && size / length < 0.25) {
+            shrink();
+        }
+        nextLast = addOne(nextLast, length);
         T tmp = Array[nextLast];
         Array[nextLast] = null;
         size --;
-        updateusage();
-
         return tmp;
     }
 }
