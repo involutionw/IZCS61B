9079a5aa42d30f91f15ce829f73b2b9b411fa193
involutionw <2651419874@qq.com>
2022-09-29 22:06:40 +0800

    OK!

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,23 +5,51 @@ public class TestArrayDequeGold {
 
     @Test
     public void Tset() {
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
-
-        for(int i = 0 ;i < 16; i ++) {
-            sad.addFirst(i);
-        }
-
-        for(int i = 15; i >= 0; i--) {
-            assertEquals(i,(int)sad.removeFirst());
-        }
-
-        for(int i = 0 ;i < 16; i ++) {
-            sad.addLast(i);
-        }
-        for(int i = 15; i >= 0; i--) {
-            int tmp = sad.removeLast();
-            assertEquals("removeLest()\n expect " + i + " but get " + tmp, i, tmp);
+        StudentArrayDeque<Integer> stad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+        String log = "";
+        for(int i = 0; i < 1000; i ++) {
+            if(stdArray.size() == 0) {
+                int number = StdRandom.uniform(1000);
+                int which = StdRandom.uniform(2);
+                if(which == 0) {
+                    stad.addFirst(number);
+                    stdArray.addFirst(number);
+                    log = log + "addFirst(" + number  +")\n";
+                }else {
+                    stad.addLast(number);
+                    stdArray.addLast(number);
+                    log = log + "addLast(" + number  +")\n";
+                }
+            }else {
+                int number, expect = 0, actual = 0;
+                int op = StdRandom.uniform(4);
+                switch(op) {
+                    case 0:
+                        expect = actual = number = StdRandom.uniform(1000);
+                        stad.addFirst(number);
+                        stdArray.addFirst(number);
+                        log = log + "addFirst(" + number  +")\n";
+                        break;
+                    case 1:
+                        expect = actual = number = StdRandom.uniform(1000);
+                        stad.addLast(number);
+                        stdArray.addLast(number);
+                        log = log + "addLast(" + number  +")\n";
+                        break;
+                    case 2:
+                        expect = stdArray.removeFirst();
+                        actual = stad.removeFirst();
+                        log = log + "removeFirst()\n";
+                        break;
+                    case 3:
+                        expect = stdArray.removeLast();
+                        actual = stad.removeLast();
+                        log = log + "removeLast()\n";
+                        break;
+                }
+                assertEquals(log, expect, actual);
+            }
         }
     }
 }
